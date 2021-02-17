02fe1d004e5264b5fc98f5cfe3c2b74acc3e64a0
tonipar <tonipar18@gmail.com>
2021-02-17 22:04:34 +0200

    comments

--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,9 @@ import os
 @app.route('/index')
 @login_required
 def index():
+    """
+    Route for mainpage. Create pagination for rounds played by user 
+    """
     page = request.args.get('page', 1, type=int)
     rounds = current_user.get_rounds().paginate(page,3,False)
     next_url = url_for('index', page=rounds.next_num) \
@@ -25,10 +28,12 @@ def index():
     return render_template('index.html', title='Home', rounds=rounds.items, next_url=next_url,
                            prev_url=prev_url)
 
-# Login Page. If user is already logged in redirect user to the main page.
-# If password or username is wrong user will get error message.
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Route for login page. Redirects to the index if user is already logged in.
+    """
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
@@ -47,12 +52,18 @@ def login():
 # Log out user from app
 @app.route('/logout')
 def logout():
+    """
+    Route for logout page.
+    """
     logout_user()
     return redirect(url_for('index'))
 
 # User registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    Route for register page.
+    """
     if current_user.is_authenticated:
         flash('curretn user authenticated')
         return redirect(url_for('index'))
@@ -70,12 +81,18 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    """
+    Route for user page.
+    """
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+    """
+    Route for edit profile page. Gets data form EditProfileForm and update current user object.
+    """
     form = EditProfileForm(current_user.username, current_user.email)
     if form.validate_on_submit():
         current_user.username = form.username.data
@@ -91,7 +108,9 @@ def edit_profile():
 @app.route('/createcourse', methods=['GET', 'POST'])
 @login_required
 def createcourse():
-
+    """
+    route for createcourse page. Gets data from CreateCourseForm and creates new course and new holes for it
+    """
     form = CreateCourseForm()
     if form.validate_on_submit():
         course = Course(coursename=form.coursename.data, courseholes=form.courseholes.data, courselocation=form.courselocation.data)
@@ -112,6 +131,9 @@ def createcourse():
 @app.route('/courses')
 @login_required
 def courses():
+    """
+    route for courses page. Creates page for all courses
+    """
     page = request.args.get('page', 1, type=int)
     courses = Course.query.order_by(Course.coursename.desc()).paginate(page,5,False)
     next_url = url_for('courses', page=courses.next_num) \
@@ -124,6 +146,9 @@ def courses():
 @app.route('/course/<coursename>')
 @login_required
 def course(coursename):
+    """
+    route for course page. 
+    """
     course = Course.query.filter_by(coursename=coursename).first_or_404()
     holes = Hole.query.filter_by(holecourse_id=course.id)
     return render_template('course.html', course=course, holes=holes)
@@ -131,6 +156,9 @@ def course(coursename):
 @app.route('/edithole/<coursename>/<holenum>', methods=['GET', 'POST'])
 @login_required
 def edithole(coursename, holenum):
+    """
+    Route for edithole page. Get data from EditHoleForm and update hole object.
+    """
     course = Course.query.filter_by(coursename=coursename).first_or_404()
     hole = Hole.query.filter_by(holenum = holenum, holecourse_id = course.id).first_or_404()
     form = EditHoleForm()
@@ -148,13 +176,17 @@ def edithole(coursename, holenum):
 @app.route('/createround', methods=['GET', 'POST'])
 @login_required
 def createround():
-
+    """
+    route for createround. gets data from CreateRoundForm and openweathermap,creates round and default values for scores.
+    """
     courses = Course.query.all()
     form = CreateRoundForm()
     if form.validate_on_submit():
         course = Course.query.filter_by(coursename=form.course.data).first_or_404()
         today = datetime.today()
-        
+        """
+        Get icon of current weather based on course location
+        """
         key = os.getenv("OWM_KEY")
         owm = OWM(key)
         mgr = owm.weather_manager()
@@ -165,7 +197,9 @@ def createround():
         round = Round(rounddate=today, roundweather=icon, rounduser_id= current_user.id ,roundcourse_id= course.id)
         db.session.add(round)
         db.session.commit()
-
+        """
+        Create default values for scores
+        """
         holes = course.get_holes()
         holenum = 1
         for hole in holes:
@@ -182,6 +216,9 @@ def createround():
 @app.route('/roundscores/<roundid>/<holenum>', methods=['GET', 'POST'])
 @login_required
 def roundscores(roundid, holenum):
+    """
+    Route for roundscores page. Check if score is not somehow created else update default score.
+    """
     round = Round.query.filter_by(id=roundid).first_or_404()
     course = Course.query.filter_by(id=round.roundcourse_id).first_or_404()
     if not isinstance(holenum, int):
@@ -218,6 +255,9 @@ def roundscores(roundid, holenum):
 @app.route('/roundview/<roundid>')
 @login_required
 def roundview(roundid):
+    """
+    route for roundview.
+    """
     round = Round.query.filter_by(id=roundid).first_or_404()
     scores = round.get_scores()
     course = Course.query.filter_by(id=round.roundcourse_id).first_or_404()
@@ -226,6 +266,9 @@ def roundview(roundid):
 @app.route('/analyzecourse/<coursename>')
 @login_required
 def analyzecourse(coursename):
+    """
+    Route for analyzecourse. Creates pages of rounds played.
+    """
     course = Course.query.filter_by(coursename=coursename).first_or_404()
     page = request.args.get('page', 1, type=int)
     rounds = course.get_rounds(current_user.id).paginate(page,3,False)
@@ -239,6 +282,9 @@ def analyzecourse(coursename):
 @app.route('/delete/<roundid>')
 @login_required
 def delete(roundid):
+    """
+    Route for delete round. After deleting redirects to index.
+    """
     round = Round.query.filter_by(id=roundid).first()
     if round.rounduser_id == current_user.id:   
         if Round is None:
